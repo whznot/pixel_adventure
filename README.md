1e082da431157176d659ad1d50fd48e07ae5c308
whznot <vonmothur@gmail.com>
2024-08-17 13:27:05 +0200

    first commit, 1/10

---
+++ README.md
@@ -0,0 +1,2 @@
+# pixel_adventure
+source: [https://www.youtube.com/playlist?list=PLRRATgFqhVCh8qD7xmaSbwG1vfaCddvCM]
---
+++ lib/pixel_adventure.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+import 'dart:ui';
+
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:pixel_adventure/levels/level.dart';
+
+class PixelAdventure extends FlameGame {
+  @override
+  Color backgroundColor() => const Color(0xFF211F30);
+  late final CameraComponent cam;
+
+  @override
+  final world = Level();
+
+  @override
+  FutureOr<void> onLoad() {
+    cam = CameraComponent.withFixedResolution(
+        world: world, width: 640, height: 360);
+    cam.viewfinder.anchor = Anchor.topLeft;
+
+    addAll([cam, world]);
+    return super.onLoad();
+  }
+}
